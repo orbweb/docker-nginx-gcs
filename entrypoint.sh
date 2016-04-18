16155060c083d197daab2530d916c0e588d68d08
Allan Lei <allanlei@helveticode.com>
2016-04-19 00:55:59 +0800

    Add GOOGLE_APPLICATION_CREDENTIALS

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 for bucket in $GCS_BUCKETS
 do
     mkdir -p /mnt/$bucket
-    gcsfuse --stat-cache-ttl $CACHE_TTL --key-file /.gcloud.json $bucket /mnt/$bucket
+    gcsfuse --stat-cache-ttl $CACHE_TTL --key-file ${GOOGLE_APPLICATION_CREDENTIALS:-/.gcloud.json} $bucket /mnt/$bucket
     echo "Mounted ${bucket}"
 done
 
